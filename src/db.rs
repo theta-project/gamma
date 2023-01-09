9c3b7bc9390a94bfcb2819757c37f4acece054d6
Aria <me@aria.rip>
2023-01-09 21:20:12 +0000

    switch to sqlx for async mysql

--- src/db.rs
+++ src/db.rs
@@ -3,28 +3,31 @@ use redis::aio::ConnectionManager as RedisConnectionManager;
 use tracing::{info, instrument};
 
 use crate::settings::DatabaseSettings;
+use sqlx::{mysql::MySqlPoolOptions, MySqlPool};
 
 pub struct Databases {
     redis: RedisConnectionManager,
-    pub mysql: mysql::Pool,
+    pub mysql: MySqlPool,
 }
 
 impl Databases {
     #[instrument(name = "connect_databases", skip(settings))]
     pub async fn new(settings: &DatabaseSettings) -> Result<Self> {
-        dbg!(settings);
         info!("connecting to redis");
         let redis =
             RedisConnectionManager::new(redis::Client::open(settings.redis_url.as_str())?).await?;
 
         info!("connecting to mysql");
-        let mysql = mysql::Pool::new(settings.mysql_url.as_str())?;
+        let mysql = MySqlPoolOptions::new()
+            .connect(settings.mysql_url.as_str())
+            .await?;
 
         Ok(Databases { redis, mysql })
     }
 
     pub fn redis(&self) -> RedisConnectionManager {
         // TODO: This is inefficient, we should really use a connection pool
+        // like deadpool, mobc, etc
         self.redis.clone()
     }
 }
